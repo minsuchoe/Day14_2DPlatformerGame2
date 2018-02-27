4842ca4b8ef74006c429b1ad107764e52e023200
minsu choe <choi.minsoo.msc@gmail.com>
2018-02-27 16:18:26 +0900

    Day14_2DPlatformerGame2

---
+++ Assets/PlatformFall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour {
+    public float fallDelay = 3f;
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            Invoke("Fall", fallDelay);
+    }
+
+    void Fall()
+    {
+        rb.isKinematic = false;        
+    }
+}
